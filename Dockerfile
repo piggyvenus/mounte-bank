314540ecf2823d5703b72add30873853fb1bb3c1
user00@example.com <piggyvenus@gmail.com>
2017-02-22 16:39:45 -0600

    new Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
-FROM node:4
+FROM registry.access.redhat.com/rhscl/nodejs-4-rhel7
+ENV PATH /opt/rh/rh-nodejs4/root/usr/bin:/opt/app-root/src/node_modules/.bin/:/opt/app-root/src/.npm-global/bin/:/opt/app-root/src/bin:/opt/app-root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 RUN npm install
+COPY imposters.sh /opt/app-root/src/
 EXPOSE 2525
 CMD ['mb']
---
+++ imposterRequest.sh
@@ -0,0 +1,31 @@
+curl -i -X POST -H 'Content-Type: application/json' http://localhost:2525/imposters --data '{
+  "port": 4545,
+  "protocol": "http",
+  "stubs": [{
+    "responses": [
+      { "is": { "statusCode": 400 }}
+    ],
+    "predicates": [{
+      "and": [
+        {
+          "equals": {
+            "path": "/test",
+            "method": "POST",
+            "headers": {
+              "Content-Type": "application/json"
+            }
+          }
+        },
+        {
+          "not": {
+            "contains": {
+              "body": "requiredField"
+            },
+            "caseSensitive": true
+          }
+        }
+      ]
+    }]
+  }]
+}'
+
